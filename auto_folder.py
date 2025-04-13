50f891594079e7bf320f496f2b1549d73d1fa806
sraya5 <sites.srayaa@gmail.com>
2025-04-13 06:19:41 +0300

    -

--- auto_folder.py
+++ auto_folder.py
@@ -12,12 +12,19 @@ def index2string(index: dict, lst: list):
         return '\n'.join(lst)
 
 
-def dir_play(input_path: str, func, args=(), output_path='', info_print=True, index: dict | None = None):
+def dir_play(input_path: str, func, args=(), output_path='', directories=False, info_print=True, index: dict | None = None):
     if index is None:
         index = {}
 
     if exists(input_path):
-        if isfile(input_path):
+        if isdir(input_path):
+            if info_print:
+                print(f'\ndirectory: {input_path}')
+            index[input_path] = {}
+            for entry in scandir(input_path):
+                new_output_path = join(output_path, entry.name) if output_path else output_path
+                dir_play(join(input_path, entry.name), func, args, new_output_path, directories, info_print, index[input_path])
+        if isfile(input_path) or directories:
             name = split(input_path)[1]
             if output_path:
                 result, error = func(input_path, *args, output_path)
@@ -29,13 +36,6 @@ def dir_play(input_path: str, func, args=(), output_path='', info_print=True, in
                     print(f'\t\t   {name}')
             elif error:
                 index[name] = False
-        elif isdir(input_path):
-            if info_print:
-                print(f'\ndirectory: {input_path}')
-            index[input_path] = {}
-            for entry in scandir(input_path):
-                new_output_path = join(output_path, entry.name) if output_path else output_path
-                dir_play(join(input_path, entry.name), func, args, new_output_path, info_print, index[input_path])
     else:
-        raise FileNotFoundError(f'not found such file or directory: {input_path}')
+        print(f'not found such file or directory: {input_path}')
     return index
